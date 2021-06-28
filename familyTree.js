b14ae84d3b2579d8c261c9bff9c11e6e501eb49a
Judith <25380136+jjliem@users.noreply.github.com>
2021-06-28 16:04:00 -0500

    Update familyTree.js

--- familyTree.js
+++ familyTree.js
@@ -5,18 +5,20 @@ class Relative{
     }
 
     childOf() {
-        return this.parents.name[0];
+        let tempParents = this.parents[0] + " & " + this.parents[1];
+        return tempParents;
     }
 }
 
 const mona = new Relative('Mona Simpson', ['Great Grandma', 'Great Grandpa']);
 const abe = new Relative('Abe Simpson', ['Great Grandma', 'Great Grandpa']);
 const jacqueline = new Relative('Jacqueline Bouvier', ['Great Grandma', 'Great Grandpa']);
-const clancy = new Relative('Jacqueline Bouvier', ['Great Grandma', 'Great Grandpa']);
+const clancy = new Relative('Clancy Bouvier', ['Great Grandma', 'Great Grandpa']);
 const marge = new Relative('Marge Simpson', [jacqueline.name, clancy.name]);
-const homer = new Relative('Homer Simpson', [mona, abe]);
+const homer = new Relative('Homer Simpson', [mona.name, abe.name]);
 const lisa = new Relative('Lisa Simpson', [marge.name, homer.name]);
-const maggie = new Relative('Maggie Simpson', [marge, homer]);
-const bart = new Relative('Bart Simpson', [marge, homer]);
+const maggie = new Relative('Maggie Simpson', [marge.name, homer.name]);
+const bart = new Relative('Bart Simpson', [marge.name, homer.name]);
 
-console.log(lisa.childOf());
+console.log("Lisa's parents are: " + lisa.childOf());
+console.log("Marge's parents are: " + marge.childOf());
